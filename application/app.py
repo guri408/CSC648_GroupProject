01d15d4f41e7dd9340bee4764f3ebd27f970e148
Ubuntu <ubuntu@ip-172-31-13-101.us-west-1.compute.internal>
2024-02-25 06:02:25 +0000

    Deployed Flask App

--- application/app.py
+++ application/app.py
@@ -1,14 +1,42 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='../css', template_folder='../html')
+app = Flask(__name__, static_folder='./css', template_folder='./html')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#@app.route('/index.html')
+#def index():
+#    return render_template('index.html')
+
 @app.route('/about')
 def about():
-    return render_template('about/about.html')
+    return render_template('about/About.html')
+
+@app.route('/about/Justin')
+def justin():
+    return render_template('about/Justin.html')
+
+@app.route('/about/Douglas')
+def douglas():
+    return render_template('about/Douglas.html')
+
+@app.route('/about/GioJung')
+def giojung():
+    return render_template('about/GioJung.html')
+
+@app.route('/about/Gurpreet')
+def gurpreet():
+    return render_template('about/Gurpreet.html')
+
+@app.route('/about/Gursimran')
+def gursimran():
+    return render_template('about/Gursimran.html')
+
+@app.route('/about/Omar')
+def omar():
+    return render_template('about/Omar.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- application/app.wsgi
+++ application/app.wsgi
@@ -1,3 +1,5 @@
 import sys
-sys.path.insert(0, '/var/www/CSC648Project/csc648-sp24-03-team03-OmarDajani75/application')
+sys.path.insert(0, '/var/www/csc648-sp24-03-team03/application')
+sys.path.insert(1, '/var/www/csc648-sp24-03-team03/venv/lib/python3.10/site-packages')
+sys.path.insert(2, '/var/www/csc648-sp24-03-team03/venv/lib64/python3.10/site-packages')
 from app import app as application
