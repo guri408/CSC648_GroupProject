3679af3d55c0e3e7b1c3290da52b65f5b94a5979
Crific <purelyhyper9@gmail.com>
2024-05-20 06:04:51 +0000

    restrict access to compose page only when it's logged in + login page to homepage path fixed

--- application/compose.py
+++ application/compose.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template, request
 from forms import ComposeForm
+from flask_login import current_user, login_required
+import logging
 
 compose = Blueprint('compose', __name__, static_folder='./public', template_folder='./public/html')
 
 @compose.route('/Compose.html', methods=['GET'])
+@login_required
 def compose_page():
     form = ComposeForm()
     receiver_id = request.args.get('receiver_id')  # Get receiver ID from query parameter
